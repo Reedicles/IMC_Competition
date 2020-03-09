1b4027356f3ee8887e7c0eb180dd9026ccae422d
Reede Rozowsky <z5163481@ad.unsw.edu.au>
2020-03-09 11:03:04 +1100

    Committed

---
+++ q1.py
@@ -0,0 +1,21 @@
+def Reede():
+	print("Reede Coded this")
+	Joel()
+
+
+def Joel():
+	print("Joel Coded this")
+	Jonah()
+
+
+def Jonah():
+	print("Jonah Coded this")
+	print("I am making changes now")
+
+
+
+if __name__ == '__main__':
+	print("My programming skills are BOOM!")
+	print("Hopefully we will all be able to view this")
+	print("We can like collaboratively each work on our own module perhaps")
+	Reede()
